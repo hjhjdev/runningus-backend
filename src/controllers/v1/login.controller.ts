c0fb19651b8cb4e7fe012daa13dd9cd32491cb8e
HJ <hj.hjdev@gmail.com>
2022-05-13 01:53:53 +0900

    Feat: 초기 카카오톡 로그인 함수 추가

--- src/controllers/v1/login.controller.ts
+++ src/controllers/v1/login.controller.ts
@@ -1,37 +1,85 @@
+import { findOauthId, insertInitialUser } from '@assets/query';
 import Database from '@libraries/database.lib';
 import { Logger } from '@utilities/winston-logger.util';
 import { Context } from 'koa';
+import fetch from 'node-fetch';
+import { KakaoRedirectResponse } from 'types/login';
+import { findOauthIdReturn } from 'types/model';
+import { apiCall as api } from '@middlewares/api.middleware';
+import { OkPacket } from 'mysql2';
 
 /**
  * 계정 조작 컨트롤러 모음
  */
 export class LoginController {
-  public static redirectLogin(ctx: Context) {
-    // Logger.debug('in redirectLogin');
+  public static handleKakaoLogin(ctx: Context) {
+    Logger.debug('in redirectLogin');
 
-    // const { query } = ctx;
-    // const { method } = query;
+    const url = [
+      'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=',
+      process.env.REST_API_KEY,
+      '&redirect_uri=',
+      process.env.REDIRECT_HOST,
+      '/v1/redirect/kakao',
+    ].join('');
 
-    const allowed = [
-      {
-        name: 'kakao',
-        // eslint-disable-next-line max-len
-        redirect: [
-          'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=',
-          process.env.REST_API_KEY,
-          '&redirect_uri=',
-          process.env.REDIRECT_HOST,
-          '/v1/',
-        ].join(''),
+    return ctx.redirect(url);
+  }
+
+  public static async handleRedirectKakaoLogin(ctx: Context) {
+    Logger.debug('in handleKakaoLogin');
+
+    const { query } = ctx;
+    const { code } = query;
+
+    // 쿼리 타입 체크
+    if (typeof code !== 'string') return api.returnBadReqest();
+
+    Logger.debug('[code]');
+    Logger.debug(code);
+
+    // 카카오 인증 서버 쿼리
+    const body = new URLSearchParams({
+      grant_type: 'authorization_code',
+      client_id: process.env.REST_API_KEY,
+      redirect_uri: [process.env.REDIRECT_HOST, '/v1/redirect/kakao'].join(''),
+      code,
+    });
+
+    Logger.debug(body);
+
+    // id_token 요청
+    const response = await fetch('https://kauth.kakao.com/oauth/token', {
+      method: 'post',
+      body,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
       },
-    ];
+    });
 
-    const found = allowed.find((item) => item.name === method);
+    // eslint-disable-next-line camelcase
+    const { id_token: idToken } = (await response.json()) as KakaoRedirectResponse;
 
-    // Logger.debug('%o', found);
+    Logger.debug('user id_token exists in db: %o', typeof idToken !== 'undefined');
 
-    // if (!found) return ctx.throw(401, 'wow', { error: ['Non supported OAuth method'] });
+    // id_token 유무 체크
+    if (!idToken) return ctx.throw(500);
 
-    // return ctx.redirect(found.redirect);
+    // id_token(db: OAUTH_ID) 존재 여부 체크
+    const foundOuthId = await Database.query<findOauthIdReturn[]>(findOauthId, [idToken]);
+
+    // 있다면 중복 가입
+    if (foundOuthId.length > 0) return api.returnBasicRequest(false, 400, '중복된 가입입니다.');
+
+    // 없다면 먼저 DB 에 OAUTH_ID 로 가입
+    const { insertId } = await Database.query<OkPacket>(insertInitialUser, [idToken]);
+
+    // eslint-disable-next-line no-return-assign
+    return (ctx.body = {
+      ...api.returnSuccessRequest('로그인을 계속 진행 하세요.'),
+      results: {
+        uid: insertId,
+      },
+    });
   }
 }
