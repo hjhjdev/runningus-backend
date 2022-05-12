2bb3fae105d2db8a1265f8c5b20bcfc4ff46b925
HJ <hj.hjdev@gmail.com>
2022-05-13 01:51:13 +0900

    Feat: 소셜로그인 관련 쿼리 추가

--- src/assets/query.ts
+++ src/assets/query.ts
@@ -4,3 +4,21 @@ export const testQuery: Sql = `
     SELECT  *
     FROM    ${process.env.USER_TABLE};
 `;
+
+export const findOauthId: Sql = `
+    SELECT UID
+    FROM   ${process.env.USER_TABLE} AS U
+    WHERE  U.OAUTH_ID = ?;
+`;
+
+export const insertInitialUser: Sql = `
+    INSERT
+    INTO   ${process.env.USER_TABLE}
+           (
+               OAUTH_ID
+           )
+    VALUES
+           (
+               ?
+           )
+`;
