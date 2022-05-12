6f720bd5739f87fbab567c153f21f20b4c7bd1f1
HJ <hj.hjdev@gmail.com>
2022-05-13 01:54:55 +0900

    Type: User model type 추가

--- src/types/model.ts
+++ src/types/model.ts
@@ -1,2 +1,16 @@
-// export type DatabaseModels =
-//   | ;
+export interface UserModel {
+  UID?: string;
+  OAUTH_ID?: string;
+  NICK?: string;
+  SEX?: string;
+  BORN?: Date;
+  DISTANCE?: number;
+  POINT_ADDRESS?: Record<string, string>;
+  TEXT_ADDRESS?: Record<string, string>;
+  PHONE?: string;
+  MANNER_POINT?: string;
+  POWER?: string;
+  POINT?: number;
+}
+
+export type findOauthIdReturn = Pick<UserModel, 'UID'>;
